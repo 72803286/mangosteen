572e3bc8ce597decc21ab81f8ef82d90f2224323
“72803286” <774959181@qq.com>
2023-06-01 15:54:33 +0800

    css初始化

--- src/views/Welcome.tsx
+++ src/views/Welcome.tsx
@@ -1,11 +1,18 @@
 import { defineComponent } from 'vue';
 import { RouterView } from 'vue-router';
+import s from './Welcome.module.scss'
+import logo from '../assets/icons/logo.svg'
+
 export const Welcome = defineComponent({
   setup: (props, context) => {
     return () => (
-      <div>
-        Welcome组件
-        <RouterView></RouterView>
+      <div class={s.wrapper}>
+        <header>
+          <img src={logo}/>
+          <h1>山竹记账</h1>
+        </header>
+        <main><RouterView></RouterView></main>
+        <footer>buttons</footer>
       </div>
     )
   }
