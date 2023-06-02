dbc5435b8466439a5b0e7f262ca71e4731d33635
“72803286” <774959181@qq.com>
2023-06-02 13:45:39 +0800

    四个欢迎页面

--- src/components/welcome/First.tsx
+++ src/components/welcome/First.tsx
@@ -8,13 +8,13 @@ export const First = defineComponent({
     return () => (
       <div class={s.wrapper}>
         <div class={s.card}>
-          <img class={s.pig} src={w1} />
+          <img  src={w1} />
+          <h2>会挣钱<br />还要会省钱</h2>
         </div>
         <div class={s.actions}>
-          <div class={s.fake}>__</div>
           <RouterLink class={s.fake} to="/start" >跳过</RouterLink>
           <RouterLink to="/welcome/2" >下一页</RouterLink>
-          <RouterLink to="/start" >跳过</RouterLink>
+          <RouterLink class={s.fake} to="/start" >跳过</RouterLink>
         </div>
       </div>
     )
