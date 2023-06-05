7f566ed18f366205713d18170685beb4dc17b327
“72803286” <774959181@qq.com>
2023-06-05 16:57:11 +0800

    滑动切换路由+重构

--- src/components/welcome/FirstActions.tsx
+++ src/components/welcome/FirstActions.tsx
@@ -1,7 +1,6 @@
 import s from './welcome.module.scss';
 import { RouterLink } from 'vue-router';
 import { FunctionalComponent } from 'vue';
-
 export const FirstActions: FunctionalComponent = () => {
   return <div class={s.actions}>
     <RouterLink class={s.fake} to="/start" >跳过</RouterLink>
