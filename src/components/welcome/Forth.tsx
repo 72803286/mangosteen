cd607d10c1ccfc07122b6041ae0cfda05bbb5cee
“72803286” <774959181@qq.com>
2023-06-05 10:31:58 +0800

    use SVG sprites

--- src/components/welcome/Forth.tsx
+++ src/components/welcome/Forth.tsx
@@ -1,19 +1,11 @@
-import { defineComponent } from 'vue';
-import s from  "./WelcomeLayout.module.scss"
-import w4 from "../../assets/icons/w4.svg"
-import { RouterLink } from 'vue-router';
-import { WelcomeLayout } from './WelcomeLayout';
-
-export const Forth = () => {
-    return <WelcomeLayout>{{
-      icon:() => <img src={w4} />,
-      text:() => <h2>每日提醒<br />不遗漏每一笔账单</h2>,
-      buttons:() => <>
-          <RouterLink class={s.fake} to="/start" >跳过</RouterLink>
-          <RouterLink to="/start" >开始记账</RouterLink>
-          <RouterLink to="/start" >跳过</RouterLink>
-          </>
-    }}</WelcomeLayout>
-}
+import s from './welcome.module.scss';
+export const Forth = () => (
+  <div class={s.card}>
+    <svg>
+      <use xlinkHref='#cloud'></use>
+    </svg>
+    <h2>每日提醒<br />不遗漏每一笔账单</h2>
+  </div>
+)
 
 Forth.displayName = 'Forth'
--- src/components/welcome/Third.tsx
+++ src/components/welcome/Third.tsx
@@ -1,19 +1,13 @@
-import { defineComponent } from 'vue';
-import s from "./WelcomeLayout.module.scss"
-import w3 from "../../assets/icons/w3.svg"
-import { RouterLink } from 'vue-router';
-import { WelcomeLayout } from './WelcomeLayout';
-
+import s from './welcome.module.scss';
 export const Third = () => {
-  return <WelcomeLayout>{{
-    icon: () => <img src={w3} />,
-    text: () => <h2>每日提醒<br />不遗漏每一笔账单</h2>,
-    buttons: () => <>
-      <RouterLink class={s.fake} to="/start" >跳过</RouterLink>
-      <RouterLink to="/welcome/4" >下一页</RouterLink>
-      <RouterLink to="/start" >跳过</RouterLink>
-    </>
-  }}</WelcomeLayout>
-
+  return (
+    <div class={s.card}>
+      <svg>
+        <use xlinkHref='#chart'></use>
+      </svg>
+      <h2>每日提醒<br />不遗漏每一笔账单</h2>
+    </div>
+  )
 }
+
 Third.displayName = 'Third'
