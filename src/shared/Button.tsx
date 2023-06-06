3ec1c772016659de41f41f91f8ae45c5e3cecff4
“72803286” <774959181@qq.com>
2023-06-06 14:32:02 +0800

    完成floatbutton

--- src/shared/Button.tsx
+++ src/shared/Button.tsx
@@ -1,8 +1,14 @@
 import { defineComponent } from 'vue';
-export const Button = defineComponent({
-  setup: (props,context) => {
+import s from './Button.module.scss';
+
+interface Props{
+  onClick: (e: MouseEvent) => void
+}
+
+export const Button = defineComponent<Props>({
+  setup: (props, context) => {
     return () => (
-      <button>
+      <button class={s.button}>
         {context.slots.default?.()}
       </button>
     )
